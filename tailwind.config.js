bacf15cdb222523b6ce667215f0647fa09f27002
Varun Jain <varunjain1224@gmail.com>
2021-05-22 12:31:57 -0700

    final commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        'gray-900-spotify':'#121212',
+        'gray-800-spotify':'#181818',
+        'gray-700-spotify':'#282828',
+      },
+      lineHeight:{
+        'extra-loose':'2.5',
+      },
+      spacing:{
+        '22':'5.5rem',
+        '200':'50rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
